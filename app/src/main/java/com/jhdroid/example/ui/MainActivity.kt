2121e4ef33e064a53f965dc297fa41c17f73deaf
JhDroid <lorzx77@gmail.com>
2021-11-15 22:00:29 +0900

    motionLayout 테스트 코드 추가

--- app/src/main/java/com/jhdroid/example/ui/MainActivity.kt
+++ app/src/main/java/com/jhdroid/example/ui/MainActivity.kt
@@ -12,11 +12,13 @@ import android.R.id
 import android.R.id.tabs
 import androidx.fragment.app.Fragment
 import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
+import com.jhdroid.example.databinding.MotionScaleBinding
 
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+//    private val binding by lazy { MotionScaleBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,24 +28,5 @@ class MainActivity : AppCompatActivity() {
             .beginTransaction()
             .replace(binding.flFragmentContainer.id, MainFragment())
             .commit()
-
-//        binding.tlTabBar.apply {
-//            addTab(newTab().setText("탭1번"))
-//            addTab(newTab().setText("탭2번"))
-//
-//            setOnTabSelectedListener(object : OnTabSelectedListener {
-//                override fun onTabSelected(tab: TabLayout.Tab) {
-//                    val selected = if (tab.position == 0) TabOneFragment() else TabTwoFragment()
-//
-//                    supportFragmentManager
-//                        .beginTransaction()
-//                        .replace(binding.flFragmentContainer.id, selected)
-//                        .commit()
-//                }
-//
-//                override fun onTabUnselected(tab: TabLayout.Tab) {}
-//                override fun onTabReselected(tab: TabLayout.Tab) {}
-//            })
-//        }
     }
 }
